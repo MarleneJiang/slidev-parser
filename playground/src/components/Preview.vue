32e1298cf09237ea0cfaec3318e66c6a72467ee8
少轻狂 <1677568218@qq.com>
2025-02-11 00:13:46 +0800

    feat: 新增自定义滚动条样式

--- playground/src/components/Preview.vue
+++ playground/src/components/Preview.vue
@@ -23,5 +23,23 @@ const inputSlides = computed(() => SlideRenderer.parse(inputMDC.value))
 .wrapper {
   height: 100%;
   width: 100%;
+  overflow: auto;
+}
+
+/* 新增自定义滚动条样式 */
+.wrapper::-webkit-scrollbar {
+  width: 8px;
+  height: 8px;
+}
+.wrapper::-webkit-scrollbar-corner {
+  background-color: var(--cm-ttc-c-track);
+}
+.wrapper::-webkit-scrollbar-thumb {
+  background-color: var(--cm-ttc-c-thumb);
+  border-radius: 3px;
+  border: 2px solid var(--cm-ttc-c-thumb);
+}
+.wrapper::-webkit-scrollbar-track {
+  background: var(--cm-ttc-c-track);
 }
 </style>
